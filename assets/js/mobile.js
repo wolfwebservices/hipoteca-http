ecd0348778e9b76637dd6d24fb085cfd4c196191
Wolf Web Services <wolfwebservices.corp@gmail.com>
2017-07-02 05:57:51 -0500

    oh shit it worked

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -2,16 +2,18 @@ var state = false;
 function openClose(){
   var listener = document.getElementById("mobile-nav-bar");
   var desktopBar = document.getElementById("nav-bar-desktop");
+var hamburger = document.getElementById("hamburger-menu");
 
   if (!state){
 removeClass(listener, "closed");
 removeClass(desktopBar, "nav-desktop-closed");
 addClass(listener, "open");
 addClass(desktopBar, "nav-desktop-open");
-
+addClass(hamburger, "is-active");
     state = true;
   }else{
     removeClass(listener, "open");
+    removeClass(hamburger, "is-active");
     removeClass(desktopBar, "nav-desktop-open");
     addClass(listener, "closed");
     addClass(desktopBar, "nav-desktop-closed");
@@ -24,6 +26,8 @@ addClass(desktopBar, "nav-desktop-open");
 function closebar(){
   var listener = document.getElementById("mobile-nav-bar");
   var desktopBar = document.getElementById("nav-bar-desktop");
+  var hamburger = document.getElementById("hamburger-menu");
+  removeClass(hamburger, "is-active");
   removeClass(listener, "open");
   removeClass(desktopBar, "nav-desktop-open");
   addClass(desktopBar, "nav-desktop-closed");
