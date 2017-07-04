350d63b7eb30dc4af0bedd0ab93be8f5afefe104
opticaregia <opticaregia112233@gmail.com>
2017-07-03 23:26:13 -0500

    Pasti wuz here
    Pasti wuz here

--- assets/js/chart-functions.js
+++ assets/js/chart-functions.js
@@ -1,41 +1,60 @@
 
 
 function loadChart(){
-  var ctx = document.getElementById("myChart");
-  var myChart = new Chart(ctx, {
-      type: 'bar',
+    var ctx = document.getElementById("chart");
+    var chart = new Chart(ctx, {
+      type:  "bar",
       data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
+        labels: ["HSBC", "Bancomer", "Santander", "Inbursa", "Scotiabank", "Banorte", "Afirme"],
+        datasets: [{
+          data: [11.10, 11.40, 11.40, 12.00, 12.20, 12.80, 14.01],
+          backgroundColor : [
+                'rgba(219, 13, 22, 0.5)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(219, 13, 22, 0.8)',
+                'rgba(3, 47, 92, 0.6)',
+                'rgba(255, 3, 2, 0.6)',
+                'rgba(152, 0, 0, 1)',
+                'rgba(0, 164, 51,  0.2)'
+            ],
+            borderColor:[
+              'rgba(0, 0, 0, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(109, 110, 112, 1)',
+              'rgba(3, 47, 92, 1)',
+              'rgba(255, 0, 0, 1)',
+              'rgba(152, 0, 0, 1)',
+              'rgba(0, 164, 51,  1)'
+            ],
+            borderWidth: 1
+        }]
       },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
+      options:{
+        title: {
+          display: true,
+          fontSize: 20,
+          text: "Porcentaje de tasa de interes"
+        },
+        legend:{
+          display: false
+        },
+        scales:{
+          yAxes:[{
+            ticks:{
+              beginAtZero: true
+            }
+          }]
+        }
       }
-  });
+    });
+    ctx.addEventListener("click", function(e){
+      var ac = chart.getElementsAtEvent(e);
+      if (ac.length>0){
+        var index = ac[0]["_index"];
+        var val = chart.data.datasets[0].data[index];
+        var label = chart.data.labels[index];
+        alert("Ha seleccionado la tasa de interes de " + label);
+        document.getElementById("interes").value = val;
+      }
+    });
 }
